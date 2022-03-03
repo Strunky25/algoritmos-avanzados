68f3ff943280647ba081a8b107f548f2f41c3fbd
JSansoP <61268003+JSansoP@users.noreply.github.com>
2022-03-03 16:57:34 +0100

    Funcions asimptotiques

--- Capitulo1/src/capitulo1/model/Model.java
+++ Capitulo1/src/capitulo1/model/Model.java
@@ -12,36 +12,76 @@ import java.util.logging.Logger;
  * @author elsho
  */
 public class Model {
-    
+
     public static final String SQRT = "sqrt(n)", LOG = "log(n)", NLOG = "n*log(n)", N2 = "n^2";
     public static final String[] COMPLEXITIES = {SQRT, LOG, NLOG, N2};
-    
+    public static final int[] SIZES = {50, 100, 150, 200};
 
-    
-    public void execute(String complexity){
+    public void execute(String complexity) {
         //System.out.println(complexity);
-        switch(complexity){
-            case SQRT -> sqrt();
-            case LOG -> log();
-            case NLOG -> nlog();
-            case N2 -> n2();
+        switch (complexity) {
+            case SQRT ->
+                sqrt();
+            case LOG ->
+                log();
+            case NLOG ->
+                nlog();
+            case N2 ->
+                n2();
         }
     }
-    
-    public void sqrt(){
-        System.out.println("hola");
-        sleep();
+
+    public void sqrt() {
+        for (int i = 0; i < SIZES.length; i++) {
+            long time = System.nanoTime();
+            for (int j = 0; j < (int) Math.sqrt(SIZES[i]); j++) {
+                sleep();
+            }
+            time = System.nanoTime() - time;
+            //View.graficar(SIZES[i],time)
+        }
     }
-    
-    public void log(){}
-    
-    public void nlog(){}
-    
-    public void n2(){}
-    
-    private void sleep(){
+
+    public void log() {
+        for (int i = 0; i < SIZES.length; i++) {
+            long time = System.nanoTime();
+            for (int j = 0; j < (int) Math.log10(SIZES[i]); j++) {
+                sleep();
+            }
+            time = System.nanoTime() - time;
+            //View.graficar(SIZES[i],time)
+        }
+    }
+
+    public void nlog() {
+        for (int i = 0; i < SIZES.length; i++) {
+            long time = System.nanoTime();
+            for (int k = 0; k < SIZES[i]; k++) {
+                for (int j = 0; j < (int) Math.log10(SIZES[i]); j++) {
+                    sleep();
+                }
+            }
+            time = System.nanoTime() - time;
+            //View.graficar(SIZES[i],time)
+        }
+    }
+
+    public void n2() {
+        for (int i = 0; i < SIZES.length; i++) {
+            long time = System.nanoTime();
+            for (int j = 0; j < SIZES[i]; j++) {
+                for(int k = 0; k < SIZES[i];k++){
+                sleep();                    
+                }
+            }
+            time = System.nanoTime() - time;
+            //View.graficar(SIZES[i],time)
+        }
+    }
+
+    private void sleep() {
         try {
-            Thread.sleep(5000);
+            Thread.sleep(5);
         } catch (InterruptedException ex) {
             Logger.getLogger(Model.class.getName()).log(Level.SEVERE, null, ex);
         }
