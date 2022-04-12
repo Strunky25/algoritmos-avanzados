7af6bfafadc5bc9fefe8db331fc403b72dbed801
Strunky25 <41077084+Strunky25@users.noreply.github.com>
2022-04-12 14:39:28 +0200

    Karapoia

--- Capitulo3/src/model/Model.java
+++ Capitulo3/src/model/Model.java
@@ -8,6 +8,7 @@
 package model;
 
 import java.math.BigInteger;
+import java.util.Collections;
 
 /**
  * Class that contains the methods that simulate the various complexities.
@@ -19,171 +20,160 @@ public class Model {
 	/* Variables */
 
 	/* Methods */
-	public String multiply(String num1, String num2) {
-		int res = 0, row = 1;
-		for (int i = num2.length() - 1; i >= 0; i--) {
-			int dig1 = Character.getNumericValue(num2.charAt(i));
-			int row1 = 1, res1 = 0;
-			for (int j = num1.length() - 1; j >= 0; j--) {
-				int dig2 = Character.getNumericValue(num1.charAt(j));
-				res1 += dig1 * dig2 * row1;
-				row1 *= 10;
-			}
-			res += res1 * row;
-			row *= 10;
-		}
-		return String.valueOf(res);
-	}
 
-	public String karatsuba(String num1, String num2) {
-		return karatsuba(new BigInteger(num1), new BigInteger(num2)).toString();
+	public String multiply(String num1, String num2) {
+		boolean isNeg = false;
+		if (num1.charAt(0) == '-') {
+			if (num2.charAt(0) == '-') {
+				num2 = num2.substring(1);
+			} else {
+				isNeg = true;
+			}
+			num1 = num1.substring(1);
+		} else if (num2.charAt(0) == '-') {
+			isNeg = true;
+			num2 = num2.substring(1);
+		}
+		String n1 = num1.length() > num2.length() ? num1 : num2;
+		String n2 = num1.length() > num2.length() ? num2 : num1;
+		String result = "0";
+		n1 = new StringBuilder(n1).reverse().toString();
+		n2 = new StringBuilder(n2).reverse().toString();
+		for (int i = 0; i < n2.length(); i++) {
+			int carry = 0;
+			int dig1 = Character.getNumericValue(n2.charAt(i));
+			String partialRes = "";
+			for (int j = 0; j < n1.length(); j++) {
+				int dig2 = Character.getNumericValue(n1.charAt(j));
+				int aux = dig1 * dig2 + carry;
+				carry = aux / 10;
+				aux = aux % 10;
+				partialRes = String.valueOf(aux) + partialRes;
+			}
+			if (carry > 0)
+				partialRes = String.valueOf(carry) + partialRes;
+			result = add(result, partialRes + String.join("", Collections.nCopies(i, "0")));
+		}
+		return isNeg ? "-" + result : result;
 	}
 
 	public String add(String num1, String num2) {
-		if (num1.length() > num2.length()) {
-			String t = num1;
-			num1 = num2;
-			num2 = t;
+		if (num1.charAt(0) == '-') {
+			if (num2.charAt(0) == '-') {
+				return "-" + add(num1.substring(1), num2.substring(1));
+			} else {
+				return sub(num2, num1.substring(1));
+			}
+		} else if (num2.charAt(0) == '-') {
+			return sub(num1, num2.substring(1));
 		}
-
-		// Take an empty String for storing result
-		String str = "";
-
-		// Calculate length of both String
-		int n1 = num1.length(), n2 = num2.length();
-
-		// Reverse both of Strings
-		num1 = new StringBuilder(num1).reverse().toString();
-		num2 = new StringBuilder(num2).reverse().toString();
-
+		String n1 = num1.length() > num2.length() ? num1 : num2;
+		String n2 = num1.length() > num2.length() ? num2 : num1;
+		String result = "";
+		int diff = Math.abs(n1.length() - n2.length());
+		n2 = String.join("", Collections.nCopies(diff, "0")) + n2;
+		n1 = new StringBuilder(n1).reverse().toString();
+		n2 = new StringBuilder(n2).reverse().toString();
 		int carry = 0;
-		for (int i = 0; i < n1; i++) {
-			// Do school mathematics, compute sum of
-			// current digits and carry
-			int sum = ((int) (num1.charAt(i) - '0') +
-					(int) (num2.charAt(i) - '0') + carry);
-			str += (char) (sum % 10 + '0');
-
-			// Calculate carry for next step
-			carry = sum / 10;
+		for (int i = 0; i < n1.length(); i++) {
+			int dig1 = Character.getNumericValue(n1.charAt(i));
+			int dig2 = Character.getNumericValue(n2.charAt(i));
+			int aux = dig1 + dig2 + carry;
+			result = String.valueOf(aux % 10) + result;
+			carry = aux / 10;
 		}
-
-		// Add remaining digits of larger number
-		for (int i = n1; i < n2; i++) {
-			int sum = ((int) (num2.charAt(i) - '0') + carry);
-			str += (char) (sum % 10 + '0');
-			carry = sum / 10;
-		}
-
-		// Add remaining carry
 		if (carry > 0)
-			str += (char) (carry + '0');
-
-		// reverse resultant String
-		str = new StringBuilder(str).reverse().toString();
-
-		return str;
+			result = String.valueOf(carry) + result;
+		return result;
 	}
 
-	public boolean isSmaller(String str1, String str2) {
-		// Calculate lengths of both string
-		int n1 = str1.length(), n2 = str2.length();
+	public boolean isSmaller(String num1, String num2) {
+		int n1 = num1.length(), n2 = num2.length();
 		if (n1 < n2)
 			return true;
 		if (n2 < n1)
 			return false;
 
 		for (int i = 0; i < n1; i++)
-			if (str1.charAt(i) < str2.charAt(i))
+			if (num1.charAt(i) < num2.charAt(i))
 				return true;
-			else if (str1.charAt(i) > str2.charAt(i))
+			else if (num1.charAt(i) > num2.charAt(i))
 				return false;
-
 		return false;
 	}
 
-	public String findDiff(String str1, String str2) {
-		// Before proceeding further, make sure str1
-		// is not smaller
-		if (isSmaller(str1, str2)) {
-			String t = str1;
-			str1 = str2;
-			str2 = t;
+	public String sub(String num1, String num2) {
+		if (num1.charAt(0) == '-') {
+			if (num2.charAt(0) == '-') {
+				return sub(num2.substring(1), num1.substring(1));
+			} else {
+				return "-" + add(num1.substring(1), num2);
+			}
+		} else if (num2.charAt(0) == '-') {
+			return add(num1, num2.substring(1));
 		}
-
-		// Take an empty string for storing result
-		String str = "";
-
-		// Calculate length of both string
-		int n1 = str1.length(), n2 = str2.length();
-
-		// Reverse both of strings
-		str1 = new StringBuilder(str1).reverse().toString();
-		str2 = new StringBuilder(str2).reverse().toString();
-
+		boolean flipped = false;
+		if (isSmaller(num1, num2)) {
+			String temp = num1;
+			num1 = num2;
+			num2 = temp;
+			flipped = true;
+		}
+		int diff = num1.length() - num2.length();
+		num2 = String.join("", Collections.nCopies(diff, "0")) + num2;
+		num1 = new StringBuilder(num1).reverse().toString();
+		num2 = new StringBuilder(num2).reverse().toString();
 		int carry = 0;
-
-		// Run loop till small string length
-		// and subtract digit of str1 to str2
-		for (int i = 0; i < n2; i++) {
-			// Do school mathematics, compute difference of
-			// current digits
-			int sub = ((int) (str1.charAt(i) - '0')
-					- (int) (str2.charAt(i) - '0') - carry);
-
-			// If subtraction is less then zero
-			// we add then we add 10 into sub and
-			// take carry as 1 for calculating next step
-			if (sub < 0) {
-				sub = sub + 10;
-				carry = 1;
+		String result = "";
+		for (int i = 0; i < num1.length(); i++) {
+			int dig1 = Character.getNumericValue(num1.charAt(i));
+			int dig2 = Character.getNumericValue(num2.charAt(i));
+			int aux = dig1 - dig2 + carry;
+			if (aux < 0) {
+				aux += 10;
+				carry = -1;
 			} else
 				carry = 0;
-
-			str += (char) (sub + '0');
+			result = String.valueOf(aux) + result;
 		}
-
-		// subtract remaining digits of larger number
-		for (int i = n2; i < n1; i++) {
-			int sub = ((int) (str1.charAt(i) - '0') - carry);
-
-			// if the sub value is -ve, then make it
-			// positive
-			if (sub < 0) {
-				sub = sub + 10;
-				carry = 1;
-			} else
-				carry = 0;
-
-			str += (char) (sub + '0');
-		}
-
-		// reverse resultant string
-		return new StringBuilder(str).reverse().toString();
+		return flipped ? "-" + result : result;
 	}
 
-	private BigInteger karatsuba(BigInteger num1, BigInteger num2) {
-		if (num1.intValue() < 10 || num2.intValue() < 10)
-			return num1.multiply(num2);
+	public String karatsuba(String num1, String num2) {
+		boolean isNeg = false;
+		if (num1.charAt(0) == '-') {
+			if (num2.charAt(0) == '-') {
+				num2 = num2.substring(1);
+			} else {
+				isNeg = true;
+			}
+			num1 = num1.substring(1);
+		} else if (num2.charAt(0) == '-') {
+			isNeg = true;
+			num2 = num2.substring(1);
+		}
+		return isNeg ? "-" + karatsubaRec(num1, num2) : karatsubaRec(num1, num2);
+	}
 
-		String num1s = num1.toString(), num2s = num2.toString();
-		int min = Math.min(num1s.length(), num2s.length());
+	private String karatsubaRec(String num1, String num2) {
+		if (num1.length() < 2 || num2.length() < 2)
+			return Integer.parseInt(num1) * Integer.parseInt(num2) + "";
+
+		int min = Math.min(num1.length(), num2.length());
 		int splitPoint = (int) Math.floor(min / 2);
 
-		BigInteger high1 = new BigInteger(num1s.substring(0, splitPoint));
-		BigInteger low1 = new BigInteger(num1s.substring(splitPoint, num1s.length()));
-		BigInteger high2 = new BigInteger(num2s.substring(0, splitPoint));
-		BigInteger low2 = new BigInteger(num2s.substring(splitPoint, num2s.length()));
-		System.out.println(high1 + " " + low1 + " " + high2 + " " + low2);
+		String high1 = num1.substring(0, splitPoint);
+		String low1 = num1.substring(splitPoint, num1.length());
+		String high2 = num2.substring(0, splitPoint);
+		String low2 = num2.substring(splitPoint, num2.length());
+		// System.out.println(high1 + " " + low1 + " " + high2 + " " + low2);
 
-		BigInteger z0 = karatsuba(low1, low2);
-		BigInteger z1 = karatsuba(new BigInteger(add(low1.toString(), high1.toString())),
-				new BigInteger(add(low2.toString(), high2.toString())));
-		BigInteger z2 = karatsuba(high2, high2);
+		String z0 = karatsubaRec(low1, low2);
+		String z1 = karatsubaRec(add(low1, high1), add(low2, high2));
+		String z2 = karatsubaRec(high1, high2);
 
-		BigInteger par1 = z2.multiply(BigInteger.TEN.pow(splitPoint * 2)); // esto no es concatenar "siplitpoint*2"
-																			// zeros?
-		BigInteger par2 = z1.subtract(z2).subtract(z0).multiply(BigInteger.TEN.pow(splitPoint));
-		return par1.add(par2).add(z0);
+		String par1 = z2 + String.join("", Collections.nCopies(splitPoint * 2, "0"));
+		String par2 = sub(sub(z1, z2), z0) + String.join("", Collections.nCopies(splitPoint, "0"));
+		return add(add(par1, par2), z0);
 	}
 }
