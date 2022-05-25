fe9e99dcad2c7608125a4676a37fd7c3d526ba6f
Strunky25 <41077084+Strunky25@users.noreply.github.com>
2022-05-25 12:10:55 +0200

    Model Commented

--- Capitulo5/src/model/Model.java
+++ Capitulo5/src/model/Model.java
@@ -15,29 +15,37 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 /**
- * Class that contains the methods that simulate the various complexities.
+ * Class that contains the word detection algorithm.
  */
 public class Model extends AbstractModel{
 
+    /* Enum of all the language dictionaries available */
     private enum Language {
         ENG,
         SPA,
         CAT;
 
+        /* Method to get dictionary file name */
         public String getDicFilename() {
             return "dics/" + this.name() + ".dic";
         }
     }
 
     /* Variables */
-    private Language lang;
-    private ArrayList<String> dictionary;
+    private Language lang; // detected language
+    private ArrayList<String> dictionary; // detected language dictionary
 
     /* Methods */
     public Model() {
         dictionary = new ArrayList<>();
     }
 
+    /**
+     * Method that loops over given words, and compares them to every word in 
+     * the detected language dictionary using levenshtein method.
+     * @param words Array of string containing words to check
+     * @return Hashmap with wrong words as key, and a list of suggestions as element.
+     */
     public HashMap<String, ArrayList<String>> correct(String[] words) {
         detectLang(words);
         dictionary = readDict(this.lang);
@@ -53,12 +61,12 @@ public class Model extends AbstractModel{
                 if (dist == 0) {
                     wrongWords.remove(word);
                     break;
-                } else {
+                } else { // Word not found on dict
                     if (dist == minDistance) {                
                         suggestions = wrongWords.get(word);
                         suggestions.add(dicWord);
                         wrongWords.put(word, (ArrayList<String>) suggestions.clone());
-                    } else if (dist < minDistance) {
+                    } else if (dist < minDistance) { // better wond found
                         minDistance = dist;  
                         wrongWords.remove(word);
                         suggestions.clear();
@@ -67,7 +75,7 @@ public class Model extends AbstractModel{
                     }
                 }
             }
-            if(wrongWords.containsKey(word)){
+            if(wrongWords.containsKey(word)){ // add word as sugestion if its right.
                 suggestions = wrongWords.get(word);
                 suggestions.add(word);
             }
@@ -77,6 +85,10 @@ public class Model extends AbstractModel{
         return wrongWords;
     }
 
+    /**
+     * Method that detects the languaje of given words.
+     * @param words Array of strings containing words
+     */
     private void detectLang(String[] words) {
         Language[] languages = Language.values();
         int[] wordsFound = new int[languages.length];
@@ -99,6 +111,11 @@ public class Model extends AbstractModel{
         this.lang = languages[max];
     }
 
+    /**
+     * Method that reads the dictionary file of a given language.
+     * @param language language to read
+     * @return ArrayList containing all words of the dictionary
+     */
     private ArrayList<String> readDict(Language language) {
         ArrayList<String> dict = new ArrayList<>();
         try (FileReader fr = new FileReader(language.getDicFilename());
@@ -114,6 +131,12 @@ public class Model extends AbstractModel{
         return dict;
     }
 
+    /**
+     * Method that calculates the levenshtein distance between two given words.
+     * @param word1 First Word to compare
+     * @param word2 Second Word to compare
+     * @return distance
+     */
     private static int levenshtein(String word1, String word2) {
         int[][] dp = new int[word1.length() + 1][word2.length() + 1];
         for (int i = 0; i <= word1.length(); i++) {
@@ -133,7 +156,12 @@ public class Model extends AbstractModel{
         return dp[word1.length()][word2.length()];
     }
 
-    static int min(int... nums) {
+    /**
+     * Aux method to find min number in a list.
+     * @param nums nums to find min
+     * @return min number
+     */
+    private static int min(int... nums) {
         return Arrays.stream(nums).min().orElse(
                 Integer.MAX_VALUE);
     }
